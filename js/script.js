2ed678440d988693c52c8084cfa8a56a746875f2
Giulio <giuliovolletti@hotmail.com>
2020-11-27 15:38:20 +0100

    commit 3

--- js/script.js
+++ js/script.js
@@ -5,29 +5,30 @@
 
 // Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età.
 var studente = {
-  nome :"marco",
-  cognome : "rossi",
+  nome :"Marco",
+  cognome : "Rossi",
   eta : 30,
 }
 // Stampare a schermo attraverso il for in tutte le proprietà;
 for (var chiave in studente ) {
   console.log(chiave + ": " + studente[chiave]);
+  document.getElementById('studente-con-proprietà').innerHTML += chiave + ": " + studente[chiave] + " ";
 }
 // Creare un array di oggetti di studenti.
 var arrayStudenti = [
   {
-    nome :"marco",
-    cognome : "polo",
+    nome :"Marco",
+    cognome : "Polo",
     eta : 30,
   },
   {
-    nome :"gianni",
-    cognome : "rossi",
+    nome :"Gianni",
+    cognome : "Rossi",
     eta : 25,
   },
   {
-    nome :"franco",
-    cognome : "danubio",
+    nome :"Franco",
+    cognome : "Danubio",
     eta : 28,
   },
 ];
@@ -35,6 +36,7 @@ console.log(arrayStudenti);
 // Ciclare su tutti gli studenti e stampare per ognuno nome e cognome;
 for (var i = 0; i < arrayStudenti.length; i++) {
   console.log("studente: ", arrayStudenti[i].nome, arrayStudenti[i].cognome);
+  document.getElementById('lista-studenti').innerHTML += "Studente: " + arrayStudenti[i].nome + " " + arrayStudenti[i].cognome + "<br>";
 }
 // Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 var arrayNuovoStudente = {}
@@ -44,3 +46,8 @@ arrayNuovoStudente.eta = parseInt(prompt("eta dello studente:"));
 arrayStudenti.push(arrayNuovoStudente)
 // console.log(arrayNuovoStudente);
 console.log(arrayStudenti);
+
+for (var i = 0; i < arrayStudenti.length; i++) {
+  console.log("studente: ", arrayStudenti[i].nome, arrayStudenti[i].cognome);
+  document.getElementById('lista-con-aggiunto').innerHTML += "Studente: " + arrayStudenti[i].nome + " " + arrayStudenti[i].cognome + "<br>";
+}
